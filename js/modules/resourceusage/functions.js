51434bd594d05ae5b16296de1d3ced7537d56d0a
Mohana-Priya <priyamohana61@gmail.com>
2014-09-10 14:41:54 +0530

    made chsnges to enable copy paste of grid cells

--- js/modules/resourceusage/functions.js
+++ js/modules/resourceusage/functions.js
@@ -1,3 +1,15 @@
+if(typeof Ext != 'undefined'){
+  Ext.core.Element.prototype.unselectable = function(){return this;};
+  Ext.view.TableChunker.metaRowTpl = [
+   '<tr class="' + Ext.baseCSSPrefix + 'grid-row {addlSelector} {[this.embedRowCls()]}" {[this.embedRowAttr()]}>',
+    '<tpl for="columns">',
+     '<td class="{cls} ' + Ext.baseCSSPrefix + 'grid-cell ' + Ext.baseCSSPrefix + 'grid-cell-{columnId} {{id}-modified} {{id}-tdCls} {[this.firstOrLastCls(xindex, xcount)]}" {{id}-tdAttr}><div class="' + Ext.baseCSSPrefix + 'grid-cell-inner ' + Ext.baseCSSPrefix + 'unselectable" style="{{id}-style}; text-align: {align};">{{id}}</div></td>',
+    '</tpl>',
+   '</tr>'
+  ];
+ } 
+
+
 function showResourcesUsage(){
 	resourcesStore.removeAll();	
 	Ext.create('Ext.grid.Panel', {
